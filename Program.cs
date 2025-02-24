106eaa566bfe64a03dff2aba1cec6297b6f02c8b
Danilo Costa <dandan_costa2222@outlook.com>
2025-02-24 17:41:32 -0300

    \Frutas-Preferidas-

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿Console.Clear();
+Console.WriteLine("### Frutas Preferidas ###");
+
+Console.WriteLine();
+
+Console.Write("1.");
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.BackgroundColor = ConsoleColor.Black;
+Console.WriteLine("Banana");
+Console.ResetColor();
+
+Console.Write("2.");
+Console.ForegroundColor = ConsoleColor.Red;
+Console.BackgroundColor = ConsoleColor.Black;
+Console.WriteLine("Maça");
+Console.ResetColor();
+
+Console.Write("3.");
+Console.ForegroundColor = ConsoleColor.DarkYellow;
+Console.BackgroundColor = ConsoleColor.Black;
+Console.WriteLine("Laranja");
+Console.ResetColor();
+
+Console.Write("4.");
+Console.ForegroundColor = ConsoleColor.DarkMagenta;
+Console.BackgroundColor = ConsoleColor.Black;
+Console.WriteLine("Uva");
+Console.ResetColor();
+
+Console.Write("5.");
+Console.ForegroundColor = ConsoleColor.DarkRed;
+Console.BackgroundColor = ConsoleColor.Black;
+Console.WriteLine("Morango");
+Console.ResetColor();
+
